6e8f427ae8f413f8a390ed7b1b89982b185b9a9e
Tomasz Wieczorek <83585@student.ue.poznan.pl>
2024-03-24 11:04:13 +0100

    dodatkowe pola

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-bok_a = 10.4
+wymyslone_a = 10.4
 
 def pole(bok_a):
     return bok_a*bok_a
 
-print(pole(bok_a))
+print(pole(wymyslone_a))
+
+
+wymyslone_b = 8.2
+
+def poleprost(bok_a,bok_b):
+    return bok_a*bok_b
+
+print(poleprost(wymyslone_a,wymyslone_b))
+
+print(pole(5))
